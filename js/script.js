9543b348074796f0057dc50cf77c3bbca1f66d3a
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-03-19 18:31:08 +0000

    added first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+function enterClicked() {
+  // input
+  const streetNumber = parseInt(document.getElementById("street-number").value)
+  const streetName = document.getElementById("street-name").value
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your address is: " + streetNumber + " "  + streetName + "."
+}
